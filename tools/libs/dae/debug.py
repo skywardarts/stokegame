067ca921b90442cf2b3912711b16d6685795ad67
Eric Muyser <eric.muyser@gmail.com>
2009-11-26 04:22:17 -0800

    Remove logging.

--- tools/libs/dae/debug.py
+++ tools/libs/dae/debug.py
@@ -7,25 +7,25 @@ class Log:
 	def __init__(self):
 		self.messages = []
 		
-		try:
-			self.log = open(sys.path[0] + "\\logs\\" + str(datetime.datetime.today().replace(microsecond = 0)) + ".log", "w")
-		except IOError:
-			self.log = False
+		###try:
+		#	self.log = open(sys.path[0] + "\\logs\\" + str(datetime.datetime.today().replace(microsecond = 0)) + ".log", "w")
+		#except IOError:
+		#	self.log = False
 			
 
-			print "Unable to create log."
+		#	print "Unable to create log."
 
 	def __del__(self):
-		if self.log:
-			self.log.close()
+		#if self.log:
+		#	self.log.close()
 	
 	def __ne__(self, message):
-		try:
-			if self.log:
-				self.log.write(message + "\n")
-				self.log.flush()
-		except IOError:
-			print "Error writing to disk."
+		#try:
+		#	if self.log:
+		#		self.log.write(message + "\n")
+		#		self.log.flush()
+		#except IOError:
+		#	print "Error writing to disk."
 			
 		print message
 
---
+++ tools/libs/dae/debug.py~
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import sys, datetime, logging
+
+
+class Log:
+	def __init__(self):
+		self.messages = []
+		
+		try:
+			self.log = open(sys.path[0] + "\\logs\\" + str(datetime.datetime.today().replace(microsecond = 0)) + ".log", "w")
+		except IOError:
+			self.log = False
+			
+
+			print "Unable to create log."
+
+	def __del__(self):
+		if self.log:
+			self.log.close()
+	
+	def __ne__(self, message):
+		try:
+			if self.log:
+				self.log.write(message + "\n")
+				self.log.flush()
+		except IOError:
+			print "Error writing to disk."
+			
+		print message
+
+log = Log()
